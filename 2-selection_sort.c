cf744a0c0242b9c0a58a001863dac17dd366f312
smithjilks <smithjilks@gmail.com>
2021-10-27 01:38:12 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,8 +21,11 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min])
 				min = j;
 
-		swap(&array[i], &array[min]);
-		print_array(array, size);
+		if (min != i)
+		{
+			swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
 	}
 }
 
